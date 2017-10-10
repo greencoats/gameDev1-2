0a8e6cb67e32039284b575a9be2b3372d414981d
Oliver Taylor <green-coats@outlook.com>
2017-10-10 16:57:13 -0400

    Working basic game
    properly compiled

---
+++ js/preload.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+let preloadState = function(){
+
+}
+
+preloadState.prototype.preload = function(){
+	game.load.image("clipboard_png", "assets/clipboard.png");
+	game.load.image("metalClipUp_png", "assets/metalClipUp.png");
+	game.load.image("metalClipDown_png", "assets/metalClipDown.png");
+}
+
+preloadState.prototype.create = function(){
+	game.state.start("Gameplay");
+}
+
+preloadState.prototype.update = function(){
+
+}
