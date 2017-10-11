84754f20f9ccdcca571fdbeb77473d4b80a08828
Claira <poiric2@rpi.edu>
2017-10-11 15:37:13 -0400

    Updated clipboard code and assets. Txt file with sources for the images used.

--- js/gameplay.js
+++ js/gameplay.js
@@ -11,8 +11,9 @@ gameplayState.prototype.create = function() {
 	this.clipboard.x = 0;
 	this.clipboard.y = 400;
 
-	this.board = this.clipboard.create(this.clipboard.x, this.clipboard.y, "clipboard_png");
-	this.metalClip = this.clipboard.create(this.clipboard.x + 185, this.clipboard.y + 55, "metalClipUp_png");
+	this.board = this.clipboard.create(this.clipboard.x, this.clipboard.y + 50, "board_png");
+	this.paper = this.clipboard.create(this.clipboard.x + 15, this.clipboard.y + 150, "paper_png");
+	this.metalClip = this.clipboard.create(this.clipboard.x + 185, this.clipboard.y, "metalClipUp_png");
 
 	this.metalClip.inputEnabled = true;
 	this.metalClip.events.onInputDown.add(this.moveClipboard, this);
@@ -26,12 +27,12 @@ gameplayState.prototype.update = function() {
 };
 
 gameplayState.prototype.moveClipboard = function() {
-	if (this.clipboard.y == -100) {
+	if (this.clipboard.y == -250) {
 		game.add.tween(this.clipboard).to( { y: 400 }, 500, Phaser.Easing.Quadratic.Out, true);
 		this.metalClip.loadTexture("metalClipUp_png");
 	}
 	else {
-		game.add.tween(this.clipboard).to( { y: -100 }, 500, Phaser.Easing.Quadratic.Out, true);
+		game.add.tween(this.clipboard).to( { y: -250 }, 500, Phaser.Easing.Quadratic.Out, true);
 		this.metalClip.loadTexture("metalClipDown_png");
 	}
 }
--- js/preload.js
+++ js/preload.js
@@ -3,9 +3,10 @@ let preloadState = function(){
 }
 
 preloadState.prototype.preload = function() {
-	game.load.image("clipboard_png", "assets/clipboard.png");
 	game.load.image("metalClipUp_png", "assets/metalClipUp.png");
 	game.load.image("metalClipDown_png", "assets/metalClipDown.png");
+	game.load.image("paper_png", "assets/paper.png");
+	game.load.image("board_png", "assets/board.png");
 }
 
 preloadState.prototype.create = function() {
