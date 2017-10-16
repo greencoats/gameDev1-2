729d9dd65c2aa84afa7028031711506fddedb4cf
Claira <poiric2@rpi.edu>
2017-10-16 17:11:59 -0400

    Speech bubble and placeholder suspect image.

--- js/gameplay.js
+++ js/gameplay.js
@@ -7,12 +7,20 @@ gameplayState.prototype.preload = function() {
 };
 
 gameplayState.prototype.create = function() {
+	this.suspect = game.add.sprite(0, 0, "poiPortrait_png");
+
+	this.speech = game.add.group();
+	this.speech.x = 0;
+	this.speech.y = 295;
+
+	this.speechBubble = this.speech.create(this.speech.x, this.speech.y + 50, "speechBubble_png");
+
 	this.clipboard = game.add.group();
 	this.clipboard.x = 0;
-	this.clipboard.y = 400;
+	this.clipboard.y = 425;
 
 	this.board = this.clipboard.create(this.clipboard.x, this.clipboard.y + 50, "board_png");
-	this.paper = this.clipboard.create(this.clipboard.x + 15, this.clipboard.y + 150, "paper_png");
+	this.paper = this.clipboard.create(this.clipboard.x + 15, this.clipboard.y + 125, "paper_png");
 	this.metalClip = this.clipboard.create(this.clipboard.x + 185, this.clipboard.y, "metalClipUp_png");
 
 	this.metalClip.inputEnabled = true;
@@ -27,12 +35,14 @@ gameplayState.prototype.update = function() {
 };
 
 gameplayState.prototype.moveClipboard = function() {
-	if (this.clipboard.y == -250) {
-		game.add.tween(this.clipboard).to( { y: 400 }, 500, Phaser.Easing.Quadratic.Out, true);
+	if (this.clipboard.y == -215) {
+		game.add.tween(this.clipboard).to( { y: 425 }, 500, Phaser.Easing.Quadratic.Out, true);
 		this.metalClip.loadTexture("metalClipUp_png");
+		game.add.tween(this.speech).to( { y: 295 }, 500, Phaser.Easing.Quadratic.Out, true);
 	}
 	else {
-		game.add.tween(this.clipboard).to( { y: -250 }, 500, Phaser.Easing.Quadratic.Out, true);
+		game.add.tween(this.clipboard).to( { y: -215 }, 500, Phaser.Easing.Quadratic.Out, true);
 		this.metalClip.loadTexture("metalClipDown_png");
+		game.add.tween(this.speech).to( { y: -345 }, 500, Phaser.Easing.Quadratic.Out, true);
 	}
 }
--- js/preload.js
+++ js/preload.js
@@ -3,10 +3,15 @@ let preloadState = function(){
 }
 
 preloadState.prototype.preload = function() {
+	// Clipboard assets
 	game.load.image("metalClipUp_png", "assets/metalClipUp.png");
 	game.load.image("metalClipDown_png", "assets/metalClipDown.png");
 	game.load.image("paper_png", "assets/paper.png");
 	game.load.image("board_png", "assets/board.png");
+
+	// Person of Interest assets
+	game.load.image("poiPortrait_png", "assets/poiPortrait.png");
+	game.load.image("speechBubble_png", "assets/speechBubble.png");
 }
 
 preloadState.prototype.create = function() {
