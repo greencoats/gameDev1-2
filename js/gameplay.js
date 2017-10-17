87468e3bebf48aa10ce28c38763fc48e63b72c82
Pheonix <colecarpenter@gmail.com>
2017-10-17 13:12:06 -0400

    Implemented

--- js/gameplay.js
+++ js/gameplay.js
@@ -13,15 +13,23 @@ gameplayState.prototype.create = function() {
 	this.currChar = 0;
 	this.currDialogues = 0;
 	this.currSegment = 0;
+	this.currIntro = 0;
 	this.currChar = 0;
 	this.maxChar = 3;
 	this.isQuestion = true;
+
+	//booleans to create modes of gameplayState
+	this.textMode = true;
+	this.questionMode = false;
+	this.isIntro = true;
+	this.isOutro = false;
+	this.isTransition = false;
 
 	//JSON Data held in array
 	this.charArr = game.cache.getJSON('character');
 	this.clipboardData = game.cache.getJSON('clipboard');
 	//get character data from JSON file and create text object
-	this.dia = game.add.text(20,300,this.charArr.characters[this.currChar].name, {fontSize: '32pt', fill:"#ffffff"});
+	this.dia = game.add.text(20,300,this.charArr.characters[this.currChar].intro[this.currIntro].question, {fontSize: '32pt', fill:"#ffffff"});
 
 	//CONTROLS
 	this.cursors = game.input.keyboard.createCursorKeys();
@@ -61,15 +69,23 @@ gameplayState.prototype.create = function() {
 };
 
 gameplayState.prototype.update = function() {
-	if(this.cursors.left.downDuration(5)){
-    this.left();
-  }
-  else if(this.cursors.right.downDuration(5)){
-    this.right();
-  }
-	game.debug.text('Time to complete dialogue: ' + (timer.duration/1000).toFixed(0), 32, 32);
-
-
+	if(this.textMode){
+		if(this.cursors.left.downDuration(5)){
+	    this.UpdateIntro();
+	  }
+	  else if(this.cursors.right.downDuration(5)){
+	    this.UpdateIntro();
+	  }
+	}
+	else if(this.questionMode){
+		if(this.cursors.left.downDuration(5)){
+	    this.left();
+	  }
+	  else if(this.cursors.right.downDuration(5)){
+	    this.right();
+	  }
+		game.debug.text('Time to complete dialogue: ' + (timer.duration/1000).toFixed(0), 32, 32);
+	}
 };
 
 gameplayState.prototype.Conclude = function(){
@@ -79,6 +95,53 @@ gameplayState.prototype.Conclude = function(){
 
 	game.state.start("Score");
 };
+
+gameplayState.prototype.UpdateIntro = function(){
+	if(this.isQuestion){
+		this.isQuestion = false;
+		this.updateCounter();
+	}
+	else if(this.currSegment < this.charArr.characters[this.currChar].intro[this.currIntro].segments.length-1){
+		this.updateClipboard();
+		this.currSegment++;
+		this.updateCounter();
+	}
+	else if (this.currIntro < this.charArr.characters[this.currChar].intro.length-1){
+		this.updateClipboard();
+		this.currSegment = 0;
+		this.currIntro++;
+		this.isQuestion = true;
+		this.updateCounter();
+	}
+	else if(!this.isTransition){
+		this.isTransition = true;
+	}
+	else{
+		this.updateClipboard();
+		if(this.isOutro){
+			this.currChar++;
+			this.isIntro = true;
+			this.isOutro = false;
+			if(this.currChar >= this.maxChar){
+				this.Conclude();
+				return;
+			}
+		}
+		if(this.isIntro){
+			this.textMode = false;
+			this.questionMode = true;
+			this.isIntro = false;
+			this.isOutro = true;
+		}
+		this.currSegment = 0;
+		this.currIntro = 0;
+		this.isQuestion = true;
+		this.isTransition = false;
+		this.updateCounter();
+	}
+	this.PrintText();
+	return;
+}
 
 gameplayState.prototype.UpdateText = function(){
 	if(this.isQuestion == true){ //Display question
@@ -97,16 +160,17 @@ gameplayState.prototype.UpdateText = function(){
 		this.isQuestion = true;
 		this.updateCounter();
 	}
+	else if(!this.isTransition){
+		this.isTransition = true;
+	}
 	else{
 		this.updateClipboard();
-		this.currChar++;
-		if(this.currChar >= this.maxChar){
-			this.Conclude();
-			return;
-		}
 		this.currSegment = 0;
 		this.currDialogues = 0;
 		this.isQuestion = true;
+		this.isTransition = false;
+		this.questionMode = false;
+		this.textMode = true;
 		this.updateCounter();
 	}
 
@@ -124,11 +188,40 @@ gameplayState.prototype.UpdateText = function(){
 };
 
 gameplayState.prototype.PrintText = function(){
-	if(this.isQuestion == true){
-		this.dia.text = this.charArr.characters[this.currChar].dialogues[this.currDialogues].question;
+	console.log(this.isIntro);
+	if(this.isTransition){
+		if(this.isIntro){
+			this.dia.text = this.charArr.characters[this.currChar].introTransition;
+		}
+		else if(this.isOutro){
+			this.dia.text = this.charArr.characters[this.currChar].questionsTransition;
+		}
 	}
-	else{
-		this.dia.text = this.charArr.characters[this.currChar].dialogues[this.currDialogues].segments[this.currSegment];
+	else if(this.textMode){
+		if(this.isIntro){
+			if(this.isQuestion == true){
+				this.dia.text = this.charArr.characters[this.currChar].intro[this.currIntro].question;
+			}
+			else{
+				this.dia.text = this.charArr.characters[this.currChar].intro[this.currIntro].segments[this.currSegment];
+			}
+		}
+		else if(this.isOutro){
+			if(this.isQuestion == true){
+				this.dia.text = this.charArr.characters[this.currChar].outro[this.currIntro].question;
+			}
+			else{
+				this.dia.text = this.charArr.characters[this.currChar].outro[this.currIntro].segments[this.currSegment];
+			}
+		}
+	}
+	else if(this.questionMode){
+		if(this.isQuestion == true){
+			this.dia.text = this.charArr.characters[this.currChar].dialogues[this.currDialogues].question;
+		}
+		else{
+			this.dia.text = this.charArr.characters[this.currChar].dialogues[this.currDialogues].segments[this.currSegment];
+		}
 	}
 };
 
