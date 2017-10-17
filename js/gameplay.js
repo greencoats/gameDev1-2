a271ce18d10c665937496573b1e04bf8e07397a0
Claira <poiric2@rpi.edu>
2017-10-16 20:52:30 -0400

    Errors in gameplay gone, going to merge with my own code.

--- js/gameplay.js
+++ js/gameplay.js
@@ -9,29 +9,6 @@ gameplayState.prototype.preload = function() {
 
 gameplayState.prototype.create = function() {
 
-	this.charData = game.cache.getJSON('character');
-	this.clipboardData = game.cache.getJSON('clipboard');
-	//get character data from JSON file and create text object
-	this.dia = game.add.text(20,300,this.charData.dialogues[0].question, {fontSize: '32pt', fill:"#ffffff"});
-
-
-	//variables to keep track of the character and statement we are on
-	this.currChar = 0;
-	this.currDialogues = 0;
-	this.currSegment = 0;
-	this.isQuestion = true;
-
-	//CONTROLS
-	this.cursors = game.input.keyboard.createCursorKeys();
-
-	//Create the timer
-	timer = game.time.create(false);
-	this.timeToCopmlete = this.charData.dialogues[this.currDialogues].timer[this.currSegment];
-	console.log("Time to complete is " + this.timeToCopmlete);
-
-	//After specified number of seconds, updateText is called
-	timer.loop(this.timeToCopmlete, function (){ this.UpdateText()}, this);
-
 	this.clipboard = game.add.group();
 	this.clipboard.x = 0;
 	this.clipboard.y = 400;
@@ -41,108 +18,33 @@ gameplayState.prototype.create = function() {
 	this.metalClip = this.clipboard.create(this.clipboard.x + 185, this.clipboard.y, "metalClipUp_png");
 
 	this.metalClip.inputEnabled = true;
-	this.metalClip.events.onInputDown.add(this.moveClipboard, this);
+	//this.metalClip.events.onInputDown.add(this.moveClipboard, this);
 
-	//Add initial synopsis text to clipboard
-	this.synopText = this.clipboardData.summaries[0][0];
-	this.synopsis = game.add.text(this.clipboard.x + 40,this.clipboard.y + 200,this.synopText,{font:'24px Arial', fill: '#ff0202', align: 'center'},this.clipboard);
-
-	//Setup abbreviation text into clipboard group
-	this.abbrev = game.add.text(this.clipboard.x + 40,this.clipboard.y + 500,"",{font:'24px Arial', fill: '#ff0202', align: 'left'},this.clipboard);
-
-	// STARS
-	this.statements = game.add.group();
-
-	//Text bubble defines
-	let textAppear = true;
-	this.textBubble = game.add.group();
-	//Set Location
-	this.textBubble.x = 50;
-	this.textBubble.y = 200;
-	//Create objects
-	this.bubble = this.textBubble.create(this.textBubble.x, this.textBubble.y, "textBubble_jpg");
-	////////////create text vairable
 	// variables used to detect and manage swipes
-	let startX;
-	let startY;
-	let endX;
-	let endY;
-	let right = false;
-	let left = false;
+	// let this.startX;
+	// let this.startY;
+	// let this.endX;
+	// let this.endY;
+	this.right = false;
+	this.left = false;
 
 	// beginSwipe function
-	game.input.onDown.add(beginSwipe, this);
+	game.input.onDown.add(this.beginSwipe, this);
 };
 
-gameplayState.prototype.UpdateText = function(){
-	console.log(this.currSegment);
-	console.log(this.charData.dialogues[this.currDialogues].segments.length);
-	if(this.isQuestion == true){ //Display question
-		this.dia.text = this.charData.dialogues[this.currDialogues].question;
-		this.isQuestion = false;
-	}
-	else if(this.currSegment < this.charData.dialogues[this.currDialogues].segments.length){
-		this.updateClipboard();
-		this.dia.text = this.charData.dialogues[this.currDialogues].segments[this.currSegment];
-		this.currSegment++;
-		this.updateCounter();
-	}
-	else if (this.currDialogues < this.charData.dialogues.length-1){
-		this.updateClipboard();
-		console.log("here");
-		this.currSegment = 0;
-		this.currDialogues++;
-		this.isQuestion = true;
-		this.UpdateText();
-		this.updateCounter();
-	}
-	else{
-		this.updateClipboard();
-		this.currChar++;
-		this.currSegment = 0;
-		this.currDialogues = 0;
-		this.isQuestion = true;
-		this.updateCounter();
-	}
-
-	//Hardcoded segment for determining when to update clipboard synopsis
-	if(this.currDialogues==1 && this.currSegment==1) {
-		this.synopsis.text += this.clipboardData.summaries[0][1];
-	}
-	else if(this.currDialogues==2 && this.currSegment==1) {
-		this.synopsis.text += this.clipboardData.summaries[0][2];
-	}
-
-	return;
-};
 //////////////////////////////////////
 // function to move the player
 gameplayState.prototype.moveBubble = function () {
 	//this.textBubble.worldX = this.textBubble.worldX + deltaX;
 	//this.bubble.y += deltaY;
-	if (right == true) {
+	if (this.right == true) {
 		this.textBubble.x = 1000;
-	} else if (left == true) {
+	} else if (this.left == true) {
 		this.textBubble.x = -1000;
 	}
-}
-
-gameplayState.prototype.right = function(){
-	this.UpdateText();
-};
-
-gameplayState.prototype.left = function(){
-	this.UpdateText();
 };
 
 gameplayState.prototype.update = function() {
-	if(this.cursors.left.downDuration(5)){
-	this.UpdateText();
-  }
-  else if(this.cursors.right.downDuration(5)){
-	this.UpdateText();
-  }
-	game.debug.text('Time to complete dialogue: ' + (timer.duration/1000).toFixed(0), 32, 32);
 
 	/////////////////////////////////
 	gameplayState.prototype.moveBubble();
@@ -161,27 +63,6 @@ gameplayState.prototype.moveClipboard = function() {
 	}
 };
 
-gameplayState.prototype.updateCounter = function() {
-	//Stop the timer
-	timer.stop();
-
-	if(this.charData.dialogues[this.currDialogues].timer[this.currSegment-1] != null) {
-		//Switch timer variable to the next value it needs to be
-		this.timeToCopmlete = this.charData.dialogues[this.currDialogues].timer[this.currSegment-1];
-		console.log("Time to complete is " + this.timeToCopmlete);
-		timer.loop(this.timeToCopmlete, function (){ this.UpdateText()}	, this);
-
-		//start the timer again
-		timer.start();
-	}
-}
-
-gameplayState.prototype.updateClipboard = function() { //function adds abbreviated statement to clipboard
-	if(this.charData.dialogues[this.currDialogues].abbr[this.currSegment-1] != null) { //Make sure the dialogue isn't currently reading a question
-		this.abbrev.text += this.charData.dialogues[this.currDialogues].abbr[this.currSegment-1];
-	}
-}
-
 //////////////////////////////////////////////////////////////////////////////////////////
 //////http://www.emanueleferonato.com/2014/11/13/html5-swipe-controlled-sokoban-game-made-with-phaser/
 	
@@ -189,49 +70,28 @@ gameplayState.prototype.updateClipboard = function() { //function adds abbreviat
 // input listener and add a new listener to be fired when the mouse/finger has been released,
 // then we call endSwipe function
 gameplayState.prototype.beginSwipe = function() {
-	startX = game.input.worldX;
-	startY = game.input.worldY;
-	game.input.onDown.remove(beginSwipe);
-	game.input.onUp.add(endSwipe);
-}
+	this.startX = game.input.worldX;
+	this.startY = game.input.worldY;
+	game.input.onDown.remove(this.beginSwipe, this);
+	game.input.onUp.add(this.endSwipe, this);
+};
 
 // function to be called when the player releases the mouse/finger
 gameplayState.prototype.endSwipe = function() {
 	// saving mouse/finger coordinates
-	endX = game.input.worldX;
-	endY = game.input.worldY;
+	this.endX = game.input.worldX;
+	this.endY = game.input.worldY;
 	// determining x and y distance travelled by mouse/finger from the start
 	// of the swipe until the end
-	var distX = startX - endX;
+	var distX = this.startX - this.endX;
 	if (distX > 0) {
-		left = true;
+		this.left = true;
 	} else if (distX < 0) {
-		right = true;
+		this.right = true;
 	}
 
 	// stop listening for the player to release finger/mouse, let's start listening for the player to click/touch
-	game.input.onDown.add(beginSwipe);
-	game.input.onUp.remove(endSwipe);
-}
+	game.input.onDown.add(this.beginSwipe, this);
+	game.input.onUp.remove(this.endSwipe, this);
 
-/*
-// function to move the player
-gameplayState.prototype.moveBubble = function () {
-	this.textBubble.worldX = this.textBubble.worldX + deltaX;
-	//this.bubble.y += deltaY;
-	if (right == true) {
-		this.textBubble.x = 1000;
-	} else if (left == true) {
-		this.textBubble.x = -1000;
-	}
-}
-*/
-
-gameplayState.prototype.textBubbleFunction = function () {
-	if (textAppear == true) {
-		game.add.tween(this.textBubble).to(100, 100, Phaser.Easing.Quadratic.Out, true);
-		this.bubble.loadTexture("textBubble_png");
-
-	}
-
-}
+};
--- js/preload.js
+++ js/preload.js
@@ -7,7 +7,9 @@ preloadState.prototype.preload = function(){
 	game.load.image("clipboard_png", "assets/clipboard.png");
 	game.load.image("metalClipUp_png", "assets/metalClipUp.png");
 	game.load.image("metalClipDown_png", "assets/metalClipDown.png");
-	game.load.image("textBubble_png", "assets/bubble.png")
+	game.load.image("textBubble_png", "assets/bubble.png");
+	game.load.image("paper_png", "assets/paper.png");
+	game.load.image("board_png", "assets/board.png");
 }
 
 preloadState.prototype.create = function(){
